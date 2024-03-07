dae7e0efdc82b0d0ff5b34452e75a710db7f15d4
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2024-03-07 11:26:41 +0100

    Added server and lib for shared schemas

---
+++ py/projects/server_x/server_x/fastapi_app.py
@@ -0,0 +1,29 @@
+import datetime
+import logging
+
+import uvicorn
+from fastapi import FastAPI
+
+import mycorp.server_schemas.schema_x as schema_x
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root() -> str:
+    return f"Alive at this time: {datetime.datetime.now()}"
+
+
+@app.get("/stuff")
+async def get_stuff() -> schema_x.Stuff:
+    stuff = schema_x.Stuff(mystr="Hello, World!", myints=[1, 2, 3])
+    return stuff
+
+# Usually, run with:
+#   uvicorn server_x.fastapi_app:app --reload
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
